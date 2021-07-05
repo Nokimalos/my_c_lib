af83ed12c534ae84ebb984a34319bed32cb04dfb
Bouldoires <bouldoires@localhost.localdomain>
2021-07-05 12:33:34 +0200

    [FIX] my_strcapitalize

--- src/my_strcapitalize.c
+++ src/my_strcapitalize.c
@@ -4,3 +4,16 @@
 ** my_strcapitalize
 */
 
+char *my_strcapitalize(char *str)
+{
+    if (str[0] >= 'a' && str[0] <= 'z')
+        str[0] -= 32;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if ((str[i] >= 'a' && str[i] <= 'z') &&
+            (str[i - 1] < 'a' || str[i - 1] > 'z') &&
+            (str[i - 1] < 'A' || str[i - 1] > 'Z') &&
+            (str[i - 1] < '0' || str[i - 1] > '9'))
+            str[i] -= 32;
+    }
+    return str;
+}
