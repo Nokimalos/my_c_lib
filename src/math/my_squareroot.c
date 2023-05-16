9466493ac71e9e90dc52b0bc103728273210d56f
Nokimalos <kaan.bouldoires@epitech.eu>
2023-05-16 18:38:10 +0200

    fix: squareroot function now is available for int, float and double

--- src/math/my_squareroot.c
+++ src/math/my_squareroot.c
@@ -4,9 +4,25 @@
 ** my_squareroot
 */
 
-int my_squareroot(int nb)
+int my_Isquareroot(int nb)
 {
     long i = 0;
+
+    for (; i * i < nb; ++i);
+    return (i * i == nb) ? i : -1;
+}
+
+float my_Fsquareroot(float nb)
+{
+    float i = 0;
+
+    for (; i * i < nb; ++i);
+    return (i * i == nb) ? i : -1;
+}
+
+double my_Dsquareroot(double nb)
+{
+    double i = 0;
 
     for (; i * i < nb; ++i);
     return (i * i == nb) ? i : -1;
